67671b2b312a5f2b333648b2a42af9bb542eb5a3
Jen <evunti@gmail.com>
2025-11-11 12:14:49 -0500

    added all pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Contact() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <main className="w-full max-w-6xl flex flex-col items-center "></main>
+    </div>
+  );
+}
